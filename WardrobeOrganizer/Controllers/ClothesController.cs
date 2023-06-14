bc8df28719655315d238c8a8b8e25ffb85fff03a
Karolina Borisova <85222435+KarolinaBorisova@users.noreply.github.com>
2023-06-14 20:11:31 +0200

    sorted by category

--- WardrobeOrganizer/Controllers/ClothesController.cs
+++ WardrobeOrganizer/Controllers/ClothesController.cs
@@ -23,16 +23,17 @@ namespace WardrobeOrganizer.Controllers
             this.logger = _logger;
         }   
 
-        public async Task<IActionResult> All(int storageId, string clothesCategory)
+        public async Task<IActionResult> All(int storageId)
         {
             
-            //find strageId
-            if (clothesCategory == null)
-            {
-               var mdel = await clothesService.AllClothes(storageId);
-                return View(mdel);
-            }
-             var model = await clothesService.AllClothes(storageId, clothesCategory);
+             var model = await clothesService.AllClothes(storageId);
+            return View(model);
+        }
+
+        public async Task<IActionResult> ClothesByCategory(int storageId, string category)
+        {
+
+            var model = await clothesService.AllClothes(storageId, category);
             return View(model);
         }
 
@@ -58,7 +59,7 @@ namespace WardrobeOrganizer.Controllers
             }
             var storageId = model.StorageId;
             int id = await clothesService.AddClothes(model);
-            return RedirectToAction("All", "Clothes", new { storageId });
+            return RedirectToAction("ClothesByCategory", "Clothes", new { storageId , model.Category});
         }
 
         public async Task<IActionResult> Details(int id)
