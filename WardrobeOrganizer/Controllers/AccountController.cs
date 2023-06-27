73abb278680dd410ee3ea7ef8a954043ec93fec4
Karolina Borisova <85222435+KarolinaBorisova@users.noreply.github.com>
2023-06-27 13:28:02 +0200

    added scaffolded register and logout

---
+++ WardrobeOrganizer/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WardrobeOrganizer.Infrastructure.Data;
+using WardrobeOrganizer.Models;
+
+namespace WardrobeOrganizer.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager; 
+
+        public AccountController(
+            UserManager<User> _userManager
+            ,SignInManager<User> _signInManager)
+        {
+            userManager = _userManager;
+            this.signInManager = _signInManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            var model = new RegisterViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model); 
+            }
+        
+            var user = new User()
+            {
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                EmailConfirmed = true,
+                UserName = model.Email
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(null, "Something went wrong");
+
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            var model = new RegisterViewModel();
+            return View(model); 
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WardrobeOrganizer/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WardrobeOrganizer.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare(nameof(Password))]
+        public string Password { get; set; }
+
+        [Required]
+        
+        public string PasswordRepeat { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 2)]
+        public string LastName { get; set; }
+    }
+}
